271cb015663f1cd43f16fba2b33cd2c485e35c39
anderquartz <iguarik25@gmail.com>
2026-01-19 12:57:07 +0300

    homework_andrey_2

---
+++ less_2/homework_2.py
@@ -0,0 +1,57 @@
+# a = 8
+# b = 3
+# x = 2.5
+# y = -1.7
+#
+# print(a)
+# print(b)
+# print(x)
+# print(y)
+#
+# print(type(a))
+# print(type(b))
+# print(type(x))
+# print(type(y))
+#
+# num1 = 10
+# num2 = 20
+#
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 // num2)
+# print(num1 % num2)
+# print(num1 ** num2)
+
+# a = 10
+# b = 3
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(' ')
+#
+# a = -10
+# b = 3
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(' ')
+#
+# a = 10
+# b = -3
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(' ')
+
+# print(5+3*2)
+# print((5+3)*2)
+# print(10/2**2)
+
+count = 10
+count+=5
+count-=3
+count*=2
+count/=4
+print(count)
