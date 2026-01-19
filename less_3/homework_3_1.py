ff678b1f85403d7882387e00eadcdd79b87aa077
anderquartz <iguarik25@gmail.com>
2026-01-19 18:13:55 +0300

    homework_andrey_2

---
+++ less_3/homework_3_1.py
@@ -0,0 +1,28 @@
+# print('hello world')
+# print(5, 10, 15)
+# print(10+25)
+
+# print(1, 2, 3, sep = " & ")
+#
+# print('Python', end=' ')
+# print('The best language')
+
+# x = 3.14
+# y = -8
+# print(f'Координаты точки: x = {x}; y = {y}')
+#
+# name = input()
+# age = input()
+# print(f'Имя: {name}, возраст: {age}')
+
+# a=input()
+# print(f'Hello, {a}!')
+
+# v1 = int(input())
+# v2 = int(input())
+# print(v1 + v2)
+
+v3 = int(input())
+print(v3 ** 2)
+
+
---
+++ less_3/homework_3_2.py
@@ -0,0 +1,36 @@
+# print(5>3)
+# print(10<2)
+# print(7==7)
+# print(6!=8)
+# print(4>=4)
+# print(9<=3)
+
+# res = 8 > 12
+# print(res)
+# print(type(res))
+
+
+# x = 15
+# print(x%2==0)
+# print(x%5==0)
+# print(x%5==0 and x%3==0)
+
+# y=4.5
+# print(y > 1 and y < 10)
+# print(0<y<5 or 10<y<15)
+# print(not(y<5))
+
+# print(bool(0))
+# print(bool(-5))
+# print(bool(3.14))
+# print(bool(''))
+# print(bool('Python'))
+# print(bool(' '))
+
+n = 4
+print(n >= 0)
+print(n % 2 == 0)
+print(n % 3 == 0)
+
+
+
